ae451475f7b13934785583acd3e137958fd1790f
Michelle <appel.michelle@gmail.com>
2016-11-01 12:12:48 +0100

    Mr. Potato Head body and checkbox. Turns visible and invisible.

--- app/src/main/java/com/example/michelle/test/MainActivity.java
+++ app/src/main/java/com/example/michelle/test/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.michelle.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       //bodyCheckBox.isChecked();
+        //itemClicked(bodyCheckBox, body);
+
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is checkbox checked
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // The image that needs to be made (in)visible
+        View image;
+        // Check which checkbox was clicked
+        if (((CheckBox) view).getId() == R.id.checkBox_body) {
+            image = findViewById(R.id.body);
+        }// else if (((CheckBox) view).getId() == R.id.checkBox_body) {
+        //    image = findViewById(R.id.body);
+        //}
+
+        // Set visibility on or of
+        if (checked) {
+            if (image.getVisibility() == View.INVISIBLE) {
+                image.setVisibility(View.VISIBLE);
+            }
+        } else {
+            if (image.getVisibility() == View.VISIBLE) {
+                image.setVisibility(View.INVISIBLE);
+            }
+        }
     }
 }
