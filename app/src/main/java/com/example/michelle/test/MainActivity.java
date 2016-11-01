a004f682f0f2d08e4fa4600115d58a6920d3a770
Michelle <appel.michelle@gmail.com>
2016-11-01 20:34:48 +0100

    Trying screen orientation

--- app/src/main/java/com/example/michelle/test/MainActivity.java
+++ app/src/main/java/com/example/michelle/test/MainActivity.java
@@ -4,44 +4,58 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.app.Activity;
+import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    View[] checkBoxList;
+
+    View[] imageList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkBoxList = new View[]{findViewById(R.id.checkBox_nose), findViewById(R.id.checkBox_eyes),
+                findViewById(R.id.checkBox_eyebrows), findViewById(R.id.checkBox_mouth),
+                findViewById(R.id.checkBox_arms), findViewById(R.id.checkBox_ears),
+                findViewById(R.id.checkBox_glasses), findViewById(R.id.checkBox_hat),
+                findViewById(R.id.checkBox_mustache), findViewById(R.id.checkBox_shoes)};
+
+        imageList = new View[]{findViewById(R.id.nose), findViewById(R.id.eyes),
+                findViewById(R.id.eyebrows), findViewById(R.id.mouth),
+                findViewById(R.id.arms), findViewById(R.id.ears),
+                findViewById(R.id.glasses), findViewById(R.id.hat),
+                findViewById(R.id.mustache), findViewById(R.id.shoes)};
+
+        // Loop trough all checkboxes to see if checked and adapt image to it
+        for (int i = 0; i < checkBoxList.length; i++) {
+            onCheckboxClicked(checkBoxList[i]);
+        }
+
+    }
+
+    public void screenOrientation() {
+        for (int i = 0; i < checkBoxList.length; i++) {
+            onCheckboxClicked(checkBoxList[i]);
+        }
     }
 
     public void onCheckboxClicked(View view) {
         // Is checkbox checked
-        boolean checked = ((CheckBox) view).isChecked();
+        boolean checked = ((CheckBox)view).isChecked();
 
         // The image that needs to be made (in)visible
         View image = null;
         // Check which checkbox was clicked
-        if (view.getId() == R.id.checkBox_nose) {
-            image = findViewById(R.id.nose);
-        } else if (view.getId() == R.id.checkBox_eyes) {
-            image = findViewById(R.id.eyes);
-        } else if (view.getId() == R.id.checkBox_eyebrows) {
-            image = findViewById(R.id.eyebrows);
-        } else if (view.getId() == R.id.checkBox_mouth) {
-            image = findViewById(R.id.mouth);
-        } else if (view.getId() == R.id.checkBox_arms) {
-            image = findViewById(R.id.arms);
-        } else if (view.getId() == R.id.checkBox_ears) {
-            image = findViewById(R.id.ears);
-        } else if (view.getId() == R.id.checkBox_glasses) {
-            image = findViewById(R.id.glasses);
-        } else if (view.getId() == R.id.checkBox_hat) {
-            image = findViewById(R.id.hat);
-        } else if (view.getId() == R.id.checkBox_mustache) {
-            image = findViewById(R.id.mustache);
-        } else if (view.getId() == R.id.checkBox_shoes) {
-            image = findViewById(R.id.shoes);
+        for (int i = 0; i < checkBoxList.length; i++) {
+            if (checkBoxList[i].getId() == view.getId()) {
+                image = imageList[i];
+            }
         }
-
         // Set visibility on or of
         if (image != null) {
             if (checked) {
