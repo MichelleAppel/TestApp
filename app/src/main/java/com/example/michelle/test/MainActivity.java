1d3f0b27ee99f8a569f2d8a40e614ba2a185a557
Michelle <appel.michelle@gmail.com>
2016-11-01 15:23:31 +0100

    Aligned checkboxes

--- app/src/main/java/com/example/michelle/test/MainActivity.java
+++ app/src/main/java/com/example/michelle/test/MainActivity.java
@@ -24,27 +24,25 @@ public class MainActivity extends AppCompatActivity {
         // The image that needs to be made (in)visible
         View image = null;
         // Check which checkbox was clicked
-        if (((CheckBox) view).getId() == R.id.checkBox_body) {
-            image = findViewById(R.id.body);
-        } else if (((CheckBox) view).getId() == R.id.checkBox_nose) {
+        if (view.getId() == R.id.checkBox_nose) {
             image = findViewById(R.id.nose);
-        } else if (((CheckBox) view).getId() == R.id.checkBox_eyes) {
+        } else if (view.getId() == R.id.checkBox_eyes) {
             image = findViewById(R.id.eyes);
-        } else if (((CheckBox) view).getId() == R.id.checkBox_eyebrows) {
+        } else if (view.getId() == R.id.checkBox_eyebrows) {
             image = findViewById(R.id.eyebrows);
-        } else if (((CheckBox) view).getId() == R.id.checkBox_mouth) {
+        } else if (view.getId() == R.id.checkBox_mouth) {
             image = findViewById(R.id.mouth);
-        } else if (((CheckBox) view).getId() == R.id.checkBox_arms) {
+        } else if (view.getId() == R.id.checkBox_arms) {
             image = findViewById(R.id.arms);
-        } else if (((CheckBox) view).getId() == R.id.checkBox_ears) {
+        } else if (view.getId() == R.id.checkBox_ears) {
             image = findViewById(R.id.ears);
-        } else if (((CheckBox) view).getId() == R.id.checkBox_glasses) {
+        } else if (view.getId() == R.id.checkBox_glasses) {
             image = findViewById(R.id.glasses);
-        } else if (((CheckBox) view).getId() == R.id.checkBox_hat) {
+        } else if (view.getId() == R.id.checkBox_hat) {
             image = findViewById(R.id.hat);
-        } else if (((CheckBox) view).getId() == R.id.checkBox_mustache) {
+        } else if (view.getId() == R.id.checkBox_mustache) {
             image = findViewById(R.id.mustache);
-        } else if (((CheckBox) view).getId() == R.id.checkBox_shoes) {
+        } else if (view.getId() == R.id.checkBox_shoes) {
             image = findViewById(R.id.shoes);
         }
 
